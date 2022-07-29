67d280eafc7601a7596fbcf0a966494181442e8b
muhrozak <rozak.tkj@gmail.com>
2022-07-29 13:51:33 +0700

    Belajar java script

---
+++ var.js
@@ -0,0 +1,19 @@
+// Variable let
+
+let iceCream = 5;
+
+console.log(iceCream);
+
+iceCream = 12;
+
+console.log(iceCream);
+
+// Variable Conts
+
+    // const iceCream = 5;
+
+    // console.log(iceCream);
+
+    // iceCream = 12;
+
+    // console.log(iceCream);
