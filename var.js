5ed10a9a35dd7b0fc7327f0f72d2528e75cafa7c
muhrozak <rozak.tkj@gmail.com>
2022-07-29 16:22:48 +0700

    menambahkn operator type data variable

--- var.js
+++ var.js
@@ -1,19 +1,54 @@
-// Variable let
+// VARIABLE
 
-let iceCream = 5;
+// LET
 
-console.log(iceCream);
+// let iceCream = 5;
 
-iceCream = 12;
+// console.log(iceCream);
 
-console.log(iceCream);
+// iceCream = 12;
 
-// Variable Conts
+// console.log(iceCream);
 
-    // const iceCream = 5;
 
-    // console.log(iceCream);
+// CONTS
 
-    // iceCream = 12;
+// const iceCream = 5;
 
-    // console.log(iceCream);
+// console.log(iceCream);
+
+// iceCream = 12;
+
+// console.log(iceCream);
+
+
+// TYPE DATA
+
+
+// const iceCream = 13;
+
+// console.log(typeof iceCream);
+
+
+// OPERATOR
+
+// const iceCream = 10 % 6;
+
+// if (iceCream > 2)
+
+//     console.log(iceCream);
+
+// else
+//     console.log("iceCream nilainya lebih rendah dari 2");
+
+
+// const sayaSukaEskrim = "YA SANGAT SUKA";
+// const sayaSukaHumburger = "APALAGI YANG INI";
+
+// console.log(sayaSukaEskrim + " " + sayaSukaHumburger);
+
+
+const sayaSukaEskrim = "YA SANGAT SUKA";
+const sayaSukaHumburger = "APALAGI YANG INI";
+
+console.log(`${sayaSukaEskrim} ${sayaSukaHumburger}`);
